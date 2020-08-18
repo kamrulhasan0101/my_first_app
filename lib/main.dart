6bbe1ef679faa0a57a390871be6641b4cda55323
kamrulhasan0101 <git.kamrulhasan0101@gmail.com>
2020-08-18 18:42:41 +0600

    Practising App bar scaffold Material wizard

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,22 @@ import "package:flutter/material.dart";
 
 void main(){
   runApp(
-    Center(
-      child:Text("Hello Kamrul Hasan", textDirection: TextDirection.ltr
-    ),)
+    MaterialApp(
+      title: "First App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("My First Flutter App"),),
+        body:       Material(
+          color: Colors.lightBlue,
+          child: Center(
+            child: Text(
+              "Hello,Mr. Kamrul",
+              textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white,fontSize: 40.0),
+            ),
+          ) ,
+        ),
+      ),
+      )
 
   );//renApp
 }
